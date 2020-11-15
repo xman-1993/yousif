1cb8f169ee81eabd8b3bdee76fdd2cd453078bff
mohamed yousif osman <74473372+xman-1993@users.noreply.github.com>
2020-11-16 00:59:40 +0200

    Add files via upload

---
+++ MOHAED.php
@@ -0,0 +1,40 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body bgcolor="grey">
+
+<center>
+<h1>بسم الله الرحمن الرحيم</h1>
+
+<?php
+$name="MOHAMED YOUSIF OSMAN";
+$class="IT 3";
+$subject="PHP";
+$addess="khartoum";
+$ID_student="123456";
+$Social_status="single";
+$Phone_number="0990379770";
+
+echo"my name is:"."$name"."<br>";
+echo"<br>";
+echo"class:"."$class"."<br>";
+echo"<br>";
+echo"subject:"."$subject"."<br>";
+echo"<br>";
+echo"addess:"."$addess"."<br>";
+echo"<br>";
+echo"ID_student:"."$ID_student"."<br>";
+echo"<br>";
+echo"Social_status:"."$Social_status"."<br>";
+echo"<br>";
+echo"Phone_number:"."$Phone_number"."<br>";
+
+?>
+
+</center>
+</body>
+</html>
